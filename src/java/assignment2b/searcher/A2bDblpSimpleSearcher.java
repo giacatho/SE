67d9728b02246e525f8581ddc7121ad7c20410d6
Tin Nguyen <giacatho@gmail.com>
2016-03-17 23:35:04 +0800

    manage to query using all the terms in terms vector, but fail because the number of term may be a lot, get BooleanQuery#TooManyCaluses exception.

--- src/java/assignment2b/searcher/A2bDblpSimpleSearcher.java
+++ src/java/assignment2b/searcher/A2bDblpSimpleSearcher.java
@@ -3,6 +3,7 @@ package assignment2b.searcher;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
@@ -10,7 +11,9 @@ import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.index.Terms;
 import org.apache.lucene.index.TermsEnum;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
@@ -21,6 +24,8 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.BytesRef;
 import se.constant.Constants;
+import se.searcher.model.Field;
+import se.searcher.model.SearchInput;
 
 /**
  *
@@ -31,7 +36,7 @@ public class A2bDblpSimpleSearcher {
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(
 				Paths.get(Constants.INDEX_ASSIGNMENT2B_DIR)));
 		IndexSearcher searcher = new IndexSearcher(reader);
-		ScoreDoc document = search(searcher, "2005", "OPODIS");
+		ScoreDoc document = search(searcher, "2012", "GLOBECOM");
 		
 		if (document == null) {
 			System.out.println("Year and venue are not found");
@@ -39,9 +44,35 @@ public class A2bDblpSimpleSearcher {
 		}
 		
 		System.out.println("Found with score " + document.score);
-		
 		printDocumentInfo(searcher, document.doc);
 		printAllTerms(searcher, document.doc, "title");
+		
+		// Document doc = searcher.doc(document.doc);
+		
+		// String title = doc.get("title");
+		
+//		if (title == null) {
+//			System.out.println("Can not get title");
+//			return;
+//		}
+		
+		//Query query = getQuery(title), 3);
+		
+		Query query = getTitleQuery(searcher, document.doc);
+		
+		TopDocs results = searcher.search(query, 3);
+		System.out.println("Near conferences:");
+		for (ScoreDoc scoreDoc : results.scoreDocs) {
+			if (scoreDoc.doc != document.doc) {
+				System.out.println("Score: " + scoreDoc.score);
+				printDocumentInfo(searcher, scoreDoc.doc);
+				printAllTerms(searcher, scoreDoc.doc, "title");
+				System.out.println("\n\n\n\n");
+			}
+		}
+		
+		//printDocumentInfo(searcher, document.doc);
+		//printAllTerms(searcher, document.doc, "title");
 	}
 	
 	public static ScoreDoc search(IndexSearcher searcher, String year, 
@@ -64,11 +95,26 @@ public class A2bDblpSimpleSearcher {
 		return results.scoreDocs[0];
 	}
 	
+	
+	
+	public static Query getQuery(String title) throws ParseException {
+
+		QueryParser parser = new QueryParser("title", new StandardAnalyzer());
+
+		title = title.substring(1, title.length()-2);
+		System.out.println(title);
+		
+		BooleanQuery.Builder queryBuilder = new BooleanQuery.Builder();
+		queryBuilder.add(parser.parse(title), BooleanClause.Occur.MUST);
+		
+		return queryBuilder.build();
+	}
+	
 	public static void printDocumentInfo(IndexSearcher searcher, int luceneDocId) throws IOException {
 		Document doc = searcher.doc(luceneDocId);
 		
 		List<IndexableField> allFields = doc.getFields();
-		System.out.println("Start to print document: ");
+		//System.out.println("Start to print document: ");
 		for (IndexableField field : allFields) {
 			System.out.println(field.name() + ": " + doc.get(field.name()));
 		}
@@ -76,7 +122,7 @@ public class A2bDblpSimpleSearcher {
 	
 	public static void printAllTerms(IndexSearcher searcher, int luceneDocId, String field) 
 			throws IOException {
-		Terms terms = searcher.getIndexReader().getTermVector(1, field);
+		Terms terms = searcher.getIndexReader().getTermVector(luceneDocId, field);
 		
 		TermsEnum termsEnum = terms.iterator();
 		BytesRef text;
@@ -84,5 +130,25 @@ public class A2bDblpSimpleSearcher {
 		while((text = termsEnum.next()) != null) {
 			System.out.print(text.utf8ToString() + ", ");
 		}
+		System.out.println();
+	}
+	
+	public static Query getTitleQuery(IndexSearcher searcher, int luceneDocId) throws IOException   {
+		
+		Terms terms = searcher.getIndexReader().getTermVector(luceneDocId, "title");
+		
+		TermsEnum termsEnum = terms.iterator();
+		BytesRef text;
+		//System.out.println("Start to print terms: ");
+		
+		BooleanQuery.Builder queryBuilder = new BooleanQuery.Builder();
+		
+		while((text = termsEnum.next()) != null) {
+			//System.out.print(text.utf8ToString() + ", ");
+			TermQuery tq = new TermQuery(new Term("title", text.utf8ToString().trim()));
+			queryBuilder.add(tq, BooleanClause.Occur.SHOULD);
+		}
+		
+		return queryBuilder.build();
 	}
 }
